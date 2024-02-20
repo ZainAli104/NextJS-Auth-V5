08f67133241e23b110cdfd81d55d079b962069d4
ZainAli <chzainali6363@gmail.com>
2024-02-20 22:14:02 +0500

    add => Login page UI complete

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+interface BackButtonProps {
+    href: string;
+    label: string;
+}
+
+export const BackButton = ({ label, href }: BackButtonProps) => {
+    return (
+        <Button
+            variant="link"
+            size="sm"
+            className="w-full font-normal"
+            asChild
+        >
+            <Link href={href}>{label}</Link>
+        </Button>
+    );
+};
--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -9,6 +9,8 @@ import {
     CardFooter
 } from "@/components/ui/card";
 import {Header} from "@/components/auth/header";
+import {Social} from "@/components/auth/social";
+import {BackButton} from "@/components/auth/back-button";
 
 interface CardWrapperProps {
     children: React.ReactNode;
@@ -30,9 +32,19 @@ export const CardWrapper = (
     return (
         <Card className="w-[400px] shadow-md">
             <CardHeader>
-                <Header label={headerLabel} />
+                <Header label={headerLabel}/>
             </CardHeader>
-            {children}
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton href={backButtonHref} label={backButtonLabel} />
+            </CardFooter>
         </Card>
     );
 };
