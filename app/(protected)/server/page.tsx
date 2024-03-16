a14f48947654edca830f25f1ceac6816399be115
ZainAli <chzainali6363@gmail.com>
2024-03-16 14:18:17 +0500

    complete✨ => Server & Client components

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,14 @@
+import {currentUser} from "@/lib/auth";
+import {UserInfo} from "@/components/user-info";
+
+const ServerPage = async () => {
+    const user = await currentUser();
+
+    return (
+        <div>
+            <UserInfo label="💻 Server component" user={user}/>
+        </div>
+    );
+};
+
+export default ServerPage;
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,5 @@
-import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession } from "next-auth";
+import { UserRole } from "@prisma/client";
 
 export type ExtendedUser = DefaultSession["user"] & {
     role: UserRole;
